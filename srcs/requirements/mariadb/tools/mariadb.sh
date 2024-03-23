5f43abe33f388caf2dd8447553289a87f0199d6b
NachoDonaire <nacho23donaire@gmail.com>
2024-03-23 15:33:32 +0100

    mariadb conf

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -5,20 +5,22 @@ set -e
 
 #mkdir -p /run/mariadb
 
-echo "CREATE DATABASE IF NOT EXISTS $WORDPRESS_DB_NAME;
-
-CREATE USER ""'$USER'""@'wordpress_container' IDENTIFIED BY ""'$WORDPRESS_DB_PASSWORD'"";
-GRANT ALL PRIVILEGES ON *.* TO ""'$USER'""@'wordpress_container' WITH GRANT OPTION;
-
-CREATE USER ""'$PUSER'""@'wordpress_container' IDENTIFIED BY ""'$PPSWD'"";
-GRANT SELECT, INSERT, UPDATE, DELETE ON $WORDPRESS_DB_NAME.* TO ""'$PUSER'""@'wordpress_container';" > /mdb.init
-
+/etc/init.d/mariadb start
 
 chmod +xr mdb.init
 
-mariadb < /mdb.init
+echo "create user $USER" | mariadb
 
-service mariadb start
+echo "GRANT ALL ON *.* TO '"$USER"'@'%' IDENTIFIED BY '"$WORDPRESS_DB_PASSWORD"'; FLUSH PRIVILEGES;" | mariadb
+
+echo "CREATE DATABASE IF NOT EXISTS $WORDPRESS_DB_NAME; GRANT ALL ON sitedb.* TO '"$USER"'@'%' IDENTIFIED BY '"$WORDPRESS_DB_PASSWORD"'; FLUSH PRIVILEGES" | mariadb
+
+
+#mariadb < /mdb.init
+
+#service mariadb start
 
 #/usr/sbin/mariadb
-tail -f
+/etc/init.d/mariadb stop
+
+#tail -f
