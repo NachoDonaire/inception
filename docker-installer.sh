91c5ce081b93f8f3830b0e4fc65f1aca4cbd4d34
NachoDonaire <nacho23donaire@gmail.com>
2024-05-20 16:44:44 +0100

    adding docker and docker compose installer. also .gitignore

---
+++ docker-installer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common make
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+sudo usermod -aG docker $ADMIN
+
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+echo "Docker ha sido instalado correctamente. Puede ser necesario cerrar sesión y volver a iniciarla para que los cambios surtan efecto."
